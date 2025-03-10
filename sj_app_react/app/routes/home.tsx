7db383a72a049080cc4b5511e38e9ae3c4120ed7
Alex Johnston <me@alexj.ca>
2025-03-10 08:22:21 -0600

    Work on Journal

--- sj_app_react/app/routes/home.tsx
+++ sj_app_react/app/routes/home.tsx
@@ -5,20 +5,29 @@ import sjclient, { tokenAuthParams } from "lib/clients/sj-client";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router";
 import { Card } from "primereact/card";
+import { PencilSquareIcon } from "@heroicons/react/16/solid";
 
 export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Strength Journal" },
+    { name: "description", content: "Record your workouts!" },
   ];
 }
 
 export default function Home() {
 
+  const workouts = [
+    { date: new Date() },
+    { date: new Date() },
+    { date: new Date() },
+    { date: new Date() },
+    { date: new Date() },
+  ]
+
   return (
     <div className="w-full flex justify-center">
-      <div className="bclass-name grow max-w-6xl flex flex-col px-2 py-1">
-        <span className="text-xl py-5">Welcome back, Alex</span>
+      <div className="bclass-name grow max-w-6xl flex flex-col px-2 py-1 gap-5">
+        <span className="text-xl pt-3">Welcome back, Alex</span>
         <div className="bg-gray-500 h-80 rounded-3xl py-5 px-5 flex flex-col">
           <span className="text-white text-4xl font-bold">2025-03-09</span>
           <div className="grow flex items-center justify-center text-xl italic text-white">
@@ -26,6 +35,20 @@ export default function Home() {
           </div>
           <div>
             <Button>Start Logging</Button>
+          </div>
+        </div>
+        <div className="bg-white rounded-3xl py-5 px-5 flex flex-col">
+          <h2 className="text-2xl font-bold">Journal</h2>
+          <div className="flex flex-col gap-6 pt-5">
+            {workouts.map(workout => (
+              <div className="flex gap-5 items-center">
+                <div className="w-6"><PencilSquareIcon /></div>
+                <div className="text-lg">{workout.date.toDateString()}</div>
+              </div>
+            ))}
+          </div>
+          <div className="pt-5 flex justify-center">
+            <Button outlined label="Load More" size="small" />
           </div>
         </div>
       </div>
