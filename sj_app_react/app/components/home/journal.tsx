8c7382f47bfa645c4a8f35c25781459789b80be6
Alex Johnston <me@alexj.ca>
2025-03-14 17:51:39 -0600

    useInfinite

--- sj_app_react/app/components/home/journal.tsx
+++ sj_app_react/app/components/home/journal.tsx
@@ -1,5 +1,5 @@
 import { PencilSquareIcon } from "@heroicons/react/16/solid";
-import sjclient from "lib/clients/sj-client";
+import sjclient, { useInfinite } from "lib/clients/sj-client";
 import type { components } from "lib/clients/sj-client.d";
 import { dateFormat1 } from "lib/utils";
 import { Button } from "primereact/button";
@@ -8,53 +8,50 @@ import { Link } from "react-router";
 
 export default function Journal() {
   const perPage = 5;
-  const [working, setWorking] = useState(false);
-  const [noMorePages, setNoMorePages] = useState(false);
-  const [ workouts, setWorkouts ] = useState([] as components["schemas"]["GetWorkoutsResponse"][]);
-  const [ page, setPage ] = useState(0);
-  useEffect(() => {
-    setWorking(true);
-    sjclient.GET('/api/Workouts', {
+  const [noMoreResults, setNoMoreResults] = useState(false);
+  const { data: workoutsList, error, isLoading, size, setSize } = useInfinite('/api/Workouts', (pageIndex, previousPageData) => {
+    if (previousPageData && previousPageData.length === 0) {
+      setNoMoreResults(true);
+      return null;
+    }
+
+    return {
       params: {
         query: {
-          page: page,
+          page: pageIndex,
           perPage: perPage
         }
       }
-    })
-    .then(res => {
-      if (res.response.ok) {
-        if (res.data!.length == 0) {
-          setNoMorePages(true);
-        }
-        setWorkouts(workouts.concat(res.data!));
-      }
-      setWorking(false);
-    });
-  }, [page]);
+    }
+  });
+
+  if (error) throw error;
+
   return (
     <div className="bg-white rounded-3xl py-5 px-5 flex flex-col">
       <h2 className="text-2xl font-bold">Journal</h2>
       <div className="flex flex-col gap-6 pt-5">
-        {workouts.map(workout => (
-          <div key={workout.id} className="flex gap-5 items-center">
-            <div className="w-6"><PencilSquareIcon /></div>
-            <div className="text-lg">
-              <Link to={`/workouts/${workout.id}/edit`}>
-                {dateFormat1(workout.entryDateUTC!)}
-              </Link>
+        {!isLoading && workoutsList!.map(workoutList => (
+          workoutList.map(workout => (
+            <div key={workout.id} className="flex gap-5 items-center">
+              <div className="w-6"><PencilSquareIcon /></div>
+              <div className="text-lg">
+                <Link to={`/workouts/${workout.id}/edit`}>
+                  {dateFormat1(workout.entryDateUTC!)}
+                </Link>
+              </div>
             </div>
-          </div>
+          ))
         ))}
       </div>
       <div className="pt-5 flex justify-center">
-        <Button 
-          outlined 
-          label="Load More" 
-          size="small" 
-          loading={working} 
-          onClick={() => setPage(page+1)}
-          disabled={noMorePages}
+        <Button
+          outlined
+          label="Load More"
+          size="small"
+          loading={isLoading}
+          onClick={() => setSize(size + 1)}
+          disabled={noMoreResults}
         />
       </div>
     </div>
--- sj_app_react/lib/clients/sj-client.ts
+++ sj_app_react/lib/clients/sj-client.ts
@@ -1,7 +1,7 @@
 import createClient, { type Middleware } from "openapi-fetch";
 import type { paths } from "./sj-client.d";
 import { auth0 } from "./auth0-client";
-import { createQueryHook } from "swr-openapi";
+import { createImmutableHook, createInfiniteHook, createMutateHook, createQueryHook } from "swr-openapi";
 
 const sjclient = createClient<paths>({
 });
@@ -28,4 +28,8 @@ const sjMiddleware: Middleware = {
 
 sjclient.use(sjMiddleware);
 
-export const useQuery = createQueryHook(sjclient, "sj-api");
+const prefix = "sj-api";
+
+export const useQuery = createQueryHook(sjclient, prefix);
+export const useImmutable = createImmutableHook(sjclient, prefix);
+export const useInfinite = createInfiniteHook(sjclient, prefix);
