bdf724fb49bc27332783de8c5ad7b9e2272bd05d
Alex Johnston <me@alexj.ca>
2025-03-23 09:12:07 -0600

    This seemed to work

--- sj_app_react/lib/clients/auth0-client.ts
+++ sj_app_react/lib/clients/auth0-client.ts
@@ -4,7 +4,7 @@ export const auth0 = new Auth0Client({
   domain: "dev-bs65rtlog25jigd0.us.auth0.com",
   clientId: "byMfHvaRsuKC1IGM9SLQtrIaAvS7wL5v",
   authorizationParams: {
-    redirect_uri: "https://strength-journal-poc2.vercel.app/",
+    redirect_uri: import.meta.env.VITE_REDIRECT_URI,
     audience: 'https://localhost:7080/api',
     scope: "read:current_user",
   }
